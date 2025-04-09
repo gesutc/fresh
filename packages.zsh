76ba402720841ea5aa4bb5dc5b904bcc5170bbea
Gareth <gesutc@gmail.com>
2025-04-09 13:17:40 +0100

    update

--- packages.zsh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/zsh
-paru -S bambustudio-bin brave-bin pulsar-bin openrocket freecad gimp libreoffice neofetch darktable gimp inkscape megasync-bin spotify psensor lightburn-bin syncthing proton-ge-custom-bin steam plex-desktop ffmpeg qt6-base xfce4 vulkan-radeon xf86-video-amdgpu amf-headers vulkan-headers vulkan-tools wine winegui-bin via-desktop-bin
--- paruins.zsh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/zsh
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -si
