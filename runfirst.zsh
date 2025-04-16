bc390547e5bb2b6228d8d7cb6c5c528ccd59576b
Gareth <gesutc@gmail.com>
2025-04-16 11:29:13 +0100

    scripts

--- runfirst.zsh
+++ runfirst.zsh
@@ -1,11 +1,10 @@
 #!/usr/bin/zsh
 cd ~
 sudo pacman -Syu
-sudo chsh -s /usr/bin/zsh
-zsh
+#sudo chsh -s /usr/bin/zsh
+#zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sudo pacman -R grml-zsh-config
 source ./.oh-my-zsh/themes/agnoster.zsh-theme
 cd ~
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cd ~
