ad6b194dfb2b2bca31ce335b077ef4baec90eb2a
Gareth <gesutc@gmail.com>
2025-04-16 10:24:05 +0100

    scripts

--- packages.zsh
+++ packages.zsh
@@ -1,2 +1,3 @@
 #!/usr/bin/zsh
-paru -S bambustudio-bin brave-bin pulsar-bin openrocket freecad gimp libreoffice neofetch darktable gimp inkscape megasync-bin spotify psensor lightburn-bin syncthing proton-ge-custom-bin steam plex-desktop ffmpeg qt6-base xfce4 vulkan-radeon xf86-video-amdgpu amf-headers vulkan-headers vulkan-tools wine winegui-bin via-desktop-bin yakuake
+paru -S bambustudio-bin brave-bin pulsar-bin openrocket freecad gimp libreoffice neofetch darktable gimp inkscape megasync-bin spotify psensor lightburn-bin syncthing proton-ge-custom-bin steam plex-desktop ffmpeg qt6-base xfce4 vulkan-radeon xf86-video-amdgpu amf-headers vulkan-headers vulkan-tools wine winegui-bin via-desktop-bin yakuake thunderbird
+paru -R smplayer discover
